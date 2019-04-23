022bdf35e149e2c5b3aa66a914a8972d0f765df2
Pavel V. Dimens <pdimens@live.com>
2019-04-23 12:18:51 -0500

    Update split_and_run_pbdagcon.sh
    replace `ls` with `find` to avoid "too many arguments" error

--- utility/split_and_run_pbdagcon.sh
+++ utility/split_and_run_pbdagcon.sh
@@ -10,11 +10,11 @@ reads_fasta=$3
 split_dir=$4
 
 #clear the directory first
-rm ${split_dir}/backbone-*
+rm -rf ${split_dir}/backbone-*
 
 ./split_reads_by_backbone.py -b ${backbone_fasta} -o ${split_dir} -r ${reads_fasta} -c ${consensus_fasta} 
 
-for file in $(ls ${split_dir}/*.reads.fasta); do
+for file in $(find ${split_dir} -name "*.reads.fasta"); do
     chunk=`basename $file .reads.fasta`
     #echo $chunk
     cmd="blasr -nproc 64 ${split_dir}/${chunk}.reads.fasta ${split_dir}/${chunk}.fasta -bestn 1 -m 5 -minMatch 19 -out ${split_dir}/${chunk}.mapped.m5"
