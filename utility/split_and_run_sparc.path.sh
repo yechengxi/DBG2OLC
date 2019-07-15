6b937f72b34d2776556ee772dd5804c13743c004
Pavel V. Dimens <pdimens@live.com>
2019-07-15 09:58:59 -0500

    Update split_and_run_sparc.path.sh

--- utility/split_and_run_sparc.path.sh
+++ utility/split_and_run_sparc.path.sh
@@ -3,12 +3,11 @@
 ###
 if [ $# -eq 0 ]; then
  echo "
- # USAGE: split_and_run_sparc.sh [BACKBONE_FASTA] [CONSENSUS_FASTA] [READS_FASTA] [OUTPUT_DIR] [ITERATIONS] [split_reads_by_backbone_version 1,2,3 - 3 by default]###
+ [USAGE:] split_and_run_sparc.sh [BACKBONE_FASTA] [CONSENSUS_FASTA] [READS_FASTA] [OUTPUT_DIR] [ITERATIONS] [split_reads_by_backbone_version 1,2,3 - 3 by default]###
  ## split_reads_by_backbone_version
  ## 1 = split_reads_by_backbone.py -- can cause "Too many files open" error. Good for small genomes and/or assemblies with few contigs.
  ## 2 = split_reads_by_backbone_readdict.py -- much faster, but requires enough memory to load all reads into memory. Will not cause "too many open files". Operation is a little different so order of reads.fastas can be different.
  ## 3 = split_reads_by_backbone_openclose.py -- a little slower than #1, but will not cause "too many open files". Will produce fastas in same order as #1. Make sure outdir is empty as it will append to existing files if they have the same name.
- "
  exit
 fi
 
@@ -25,9 +24,10 @@ else
 fi
 
 # if dir not there, make it; else clean the dir out
-if [ ! -d $split_dir ]; then mkdir $splitdir ; else  rm ${split_dir}/backbone-* ; fi
+rm -rf $split_dir
+mkdir -p $split_dir
 
-
+echo -e "\t-- splitting reads --"
 if [ $splitversion -eq 1 ]; then
  split_reads_by_backbone.py -b ${backbone_fasta} -o ${split_dir} -r ${reads_fasta} -c ${consensus_fasta} 
 elif [ $splitversion -eq 2 ]; then
@@ -35,11 +35,11 @@ elif [ $splitversion -eq 2 ]; then
 elif [ $splitversion -eq 3 ]; then
  split_reads_by_backbone_openclose.py -b ${backbone_fasta} -o ${split_dir} -r ${reads_fasta} -c ${consensus_fasta} 
 else
- echo "splitversion (argument #6) needs to be 1, 2, or 3. If left blank, default is 1."
+ echo "splitversion (argument #6) needs to be 1, 2, or 3. If left blank, default is 3."
  exit
 fi
 
-for file in $(ls ${split_dir}/*.reads.fasta); do
+for file in $(find ${split_dir} -name "*.reads.fasta"); do
     chunk=`basename $file .reads.fasta`
 
     cmd=""
@@ -47,7 +47,7 @@ for file in $(ls ${split_dir}/*.reads.fasta); do
 
         #echo $iter
 
-        cmd+="blasr -nproc 64 ${split_dir}/${chunk}.reads.fasta ${split_dir}/${chunk}.fasta -bestn 1 -m 5 -minMatch 19 -out ${split_dir}/${chunk}.mapped.m5; "
+        cmd="blasr --nproc 64 ${split_dir}/${chunk}.reads.fasta ${split_dir}/${chunk}.fasta --bestn 1 -m 5 --minMatch 19 --out ${split_dir}/${chunk}.mapped.m5"
 
         cmd+="Sparc m ${split_dir}/${chunk}.mapped.m5 b ${split_dir}/${chunk}.fasta k 1 c 2 g 1 HQ_Prefix Contig boost 5 t 0.2 o ${split_dir}/${chunk}; "
 
@@ -62,7 +62,6 @@ for file in $(ls ${split_dir}/*.reads.fasta); do
     echo $cmd
     eval $cmd
 
-
     #to save space
     cmd="rm ${split_dir}/${chunk}.mapped.m5"
     echo $cmd
@@ -73,6 +72,5 @@ for file in $(ls ${split_dir}/*.reads.fasta); do
 
 done
 
-cmd="cat ${split_dir}/*.consensus.fasta > ${split_dir}/final_assembly.fasta"
-echo=$cmd
-eval $cmd
+echo="merging consensus reads to ${split_dir}/final_assembly.fasta"
+for f in ${split_dir}/*.consensus.fasta; do cat "$f" >> ${split_dir}/final_assembly.fasta; done
